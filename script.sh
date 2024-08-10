626a80be05011c4905b9ad0dacb89d1fa8595298
nataliya-panina <nataliya.sabathier@gmail.com>
2024-08-10 11:45:55 +0200

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd /home/$USER/prometheus
+docker rm -f /node_exporter
+
+docker run -d --net="host" --name node_exporter -v "/:/host:ro,rslave" \
+quay.io/prometheus/node-exporter:latest --path.rootfs=/host
+
+docker compose up -d
+echo "paninang 5" | curl --data-binary @- http://localhost:9091/metrics/job/net>
+ss -tlpn
